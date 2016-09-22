31145e98bc7a122d981fc7700c7b8b59139f02c4
istiqomah <istiqomahchelsea@gmail.com>
2016-09-22 15:21:11 +0700

    quick commit

--- Simaspay DIMO/Simaspay/Base Objects/BaseButton.swift
+++ Simaspay DIMO/Simaspay/Base Objects/BaseButton.swift
@@ -22,6 +22,25 @@ class BaseButton: UIButton {
         layer.cornerRadius = 5;
         layer.masksToBounds = true;
     }
+    
+    func updateButtonType1() {
+        // background red with text white
+        self.backgroundColor = UIColor.init(hexString: color_btn_red)
+        self.layer.cornerRadius = 4;
+        self.clipsToBounds = true;
+        self.titleLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 20)
+        self.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+    }
+    
+    func updateButtonType2() {
+        // background red with text white
+        self.backgroundColor = UIColor.init(hexString: color_btn_gray)
+        self.layer.cornerRadius = 4;
+        self.clipsToBounds = true;
+        self.titleLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 20)
+        self.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+    }
+
     /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
