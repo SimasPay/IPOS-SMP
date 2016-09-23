a1bec704804ef6ed3ffce6e23a1959779b38cc3f
Kendy Susantho <kendysusantho16@gmail.com>
2016-09-23 11:06:51 +0700

    prepare for contact us, activation page

---
+++ Simaspay DIMO/Simaspay/Controllers/Login and activation/ActivationPinViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ActivationPinViewController.swift
+//  Simaspay
+//
+//  Created by Kendy Susantho on 9/23/16.
+//  Copyright © 2016 Kendy Susantho. All rights reserved.
+//
+
+import UIKit
+
+class ActivationPinViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
