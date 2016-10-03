3b71212b9442054ba4f5dd05689834fcfd50367a
istiqomah <istiqomahchelsea@gmail.com>
2016-10-03 17:15:09 +0700

    contact us prepare for simaspay

--- Simaspay DIMO/Simaspay/Controllers/Login and activation/ContactUSViewController.swift
+++ Simaspay DIMO/Simaspay/Controllers/Login and activation/ContactUSViewController.swift
@@ -9,11 +9,57 @@
 import UIKit
 
 class ContactUSViewController: BaseViewController {
+    @IBOutlet var viewMainFrame: UIView!
+
+    @IBOutlet var lblTitleTlp: BaseLabel!
+    @IBOutlet var lblTitleEmail: BaseLabel!
+    @IBOutlet var lblTitleWeb: BaseLabel!
+    
+    @IBOutlet var lblFirstTlp: BaseLabel!
+    @IBOutlet var lblSecondTlp: BaseLabel!
+    @IBOutlet var lblEmail: BaseLabel!
+    @IBOutlet var lblWeb: BaseLabel!
+    static func initWithOwnNib() -> ContactUSViewController {
+        let obj:ContactUSViewController = ContactUSViewController.init(nibName: String(describing: self), bundle: nil)
+        return obj
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.showTitle(getString("ContactUsTitle"))
+        self.showBackButton()
+        
+        self.viewMainFrame.layer.cornerRadius = 5.0;
+        self.viewMainFrame.layer.borderColor = UIColor.init(hexString: "D1D1D1").cgColor
+        self.viewMainFrame.layer.borderWidth = 0.5
+        self.viewMainFrame.clipsToBounds = true;
+        
+        
+        self.lblTitleTlp.text = "Bank Sinarmas Care"
+        self.lblTitleTlp.font = UIFont .boldSystemFont(ofSize: 13)
+        self.lblTitleEmail.text = "E-mail"
+        self.lblTitleEmail.font = self.lblTitleTlp.font
+        self.lblTitleWeb.text = "Website"
+        self.lblTitleWeb.font = self.lblTitleTlp.font
+        
+        self.lblFirstTlp.font = UIFont.boldSystemFont(ofSize: 16)
+        self.lblSecondTlp.font = self.lblFirstTlp.font
+        self.lblEmail.font = self.lblFirstTlp.font
+        self.lblWeb.font = self.lblFirstTlp.font
+        self.lblFirstTlp.text = "1500 153"
+        self.lblSecondTlp.text = "021 501 88888"
+        self.lblEmail.text = "care@banksinarmas.com"
+        self.lblWeb.text = "www.banksinarmas.com"
+        
+        
+        
+        
+
+    }
+    
+    override func viewDidLayoutSubviews() {
+        // adjust view main frame
     }
 
     override func didReceiveMemoryWarning() {
